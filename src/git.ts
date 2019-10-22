4f44ab8c06096bd2a93cddcdee88f24b7e78151c
James Ives <iam@jamesiv.es>
2019-10-22 10:48:52 -0400

    d

--- src/git.ts
+++ src/git.ts
@@ -73,9 +73,7 @@ export async function deploy() {
 
     await execute(`git fetch origin`, workspace);
     await execute(`git worktree add --checkout ${temporaryDeploymentDirectory} origin/${action.branch}`, workspace);
-    console.log(await execute(`ls`, workspace))
-    console.log(await execute(`ls ${build}`, workspace))
-    await execute(`cp -rf ~/${build} ${temporaryDeploymentDirectory}`, workspace)
+    await cp(`~${workspace}/${build}/*`, temporaryDeploymentDirectory, {recursive: true, force: true})
 
     await execute(`git add --all`, temporaryDeploymentDirectory)
     await execute(`git checkout -b ${temporaryDeploymentBranch}`, temporaryDeploymentDirectory);
