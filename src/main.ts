480de155686ffe5d26c074f2205e696fd5ac00a0
James Ives <iam@jamesiv.es>
2019-11-07 16:58:07 -0500

    Promises

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import * as core from "@actions/core";
 import { init, deploy } from "./git";
 
+/** Initializes and runs the action.
+ * @returns {undefined}
+ */
 (async function() {
   try {
     // Initializes the action.
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,11 @@
 import { exec } from "@actions/exec";
 
+/** Wrapper around the GitHub toolkit exec command which returns the output.
+ * Also allows you to easily toggle the current working directory.
+ * @param {String} cmd = The command to execute.
+ * @param {String} cwd - The current working directory.
+ * @returns {String} - The output from the command.
+ */
 export async function execute(cmd: string, cwd: string): Promise<String> {
   let output = '';
 
@@ -12,5 +18,5 @@ export async function execute(cmd: string, cwd: string): Promise<String> {
     }
   });
 
-  return output;
+  return Promise.resolve(output);
 }
